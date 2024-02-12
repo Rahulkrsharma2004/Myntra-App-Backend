16da00c54198a79077d14b89144d8a86e5c50363
RahulKrSharma2004 <kumarrahulbasdiha@gmail.com>
2024-02-12 17:40:27 +0530

    update once

--- Models/cartModel.js
+++ Models/cartModel.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const cartSchema = new mongoose.Schema(
     {
         userId: {
-            type: mongoose.Schema.Types.ObjectId,
+            type:String,
+            required:true,
         },
         productId: {
-            type: mongoose.Schema.Types.ObjectId,
+            type:String,
+            required:true,
         },
         
     },
--- Routes/cartRoute.js
+++ Routes/cartRoute.js
@@ -15,44 +15,46 @@ cartRouter.get("/", async (req, res) => {
 });
 
 
-cartRouter.post("/add", async (req, res) => {
+cartRouter.post("/add/:id", async (req, res) => {
   try {
-    const { userId, productId, quantity } = req.body;
+    const productId = req.params.id
+    const { userId } = req.body;
     const isProductExist = await CartModel.findOne({ productId, userId });
     if (isProductExist) {
       return res
         .status(404)
         .send({ message: "Product already exists in cart" });
     }
-    const cart = await CartModel.create({ userId, productId, quantity });
-    const newCartItem = await CartModel.findById(cart._id)
-      .populate("productId")
-      .select("-userId");
-    return res.status(201).send({ message: `Product Added Successfully` });
+    const product = await ProductModel.findOne({_id:productId})
+    if(product){
+      const cart =  CartModel({ userId, productId });
+      await cart.save()
+      return res.status(201).send({ message: `Product Added Successfully` });
+    } 
+    
   } catch (error) {
     return res.status(404).send({ message: "Something went wrong" });
   }
 });
 
 
-
-cartRouter.delete("/delete", async (req, res) => {
-  try {
-    const { id, userId } = req.body;
-    const cartItem = await CartModel.findById(id);
-    if (cartItem && cartItem.userId.toString() === userId) {
-      const cart = await CartModel.findByIdAndDelete(id)
-        .populate("productId")
-        .select("-userId");
-      return res
-        .status(200)
-        .send({ message: `Deleted the product from cart successfully` });
-    } else {
-      return res.status(404).send({ message: "Item does not exist in cart" });
-    }
-  } catch (error) {
-    return res.status(404).send({ message: "Something went wrong" });
-  }
-});
+// cartRouter.delete("/delete/:id", async (req, res) => {
+//   try {
+//     const { id, userId } = req.body;
+//     const cartItem = await CartModel.findById(id);
+//     if (cartItem && cartItem.userId.toString() === userId) {
+//       const cart = await CartModel.findByIdAndDelete(id)
+//         .populate("productId")
+//         .select("-userId");
+//       return res
+//         .status(200)
+//         .send({ message: `Deleted the product from cart successfully` });
+//     } else {
+//       return res.status(404).send({ message: "Item does not exist in cart" });
+//     }
+//   } catch (error) {
+//     return res.status(404).send({ message: "Something went wrong" });
+//   }
+// });
 
 module.exports = cartRouter;
