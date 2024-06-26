43f69d87764dd2796de8542b7de165758f61264d
RahulKrSharma2004 <kumarrahulbasdiha@gmail.com>
2024-06-27 01:19:19 +0530

    some thing add

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(cors({
     origin:
-    ["http://localhost:5173", "https://myntra-app-backend.vercel.app", "https://myntra-project-nu.vercel.app/"],
+    ["http://localhost:5173", "https://myntra-app-backend.vercel.app", "https://myntra-project-nu.vercel.app"],
     credentials: true,
 }));
 
